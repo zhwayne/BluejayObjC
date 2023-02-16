5d4ec870c5a7cb9b61a15c65abd52ce48ccac02e
iya <mrzhwayne@163.com>
2023-02-16 16:47:53 +0800

    first commit

---
+++ Example/Pods/Target Support Files/BluejayObjC/BluejayObjC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BluejayObjC : NSObject
+@end
+@implementation PodsDummy_BluejayObjC
+@end
---
+++ Example/Pods/Target Support Files/Pods-BluejayObjC_Tests/Pods-BluejayObjC_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BluejayObjC_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BluejayObjC_Tests
+@end
